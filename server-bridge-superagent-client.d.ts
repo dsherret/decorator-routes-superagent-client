e1aa6c3f1a24824f0ec430df6b247ba4695639e1
David Sherret <dsherret@gmail.com>
2015-11-02 00:36:43 -0500

    Remove es6 reference.

--- server-bridge-superagent-client.d.ts
+++ server-bridge-superagent-client.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="node_modules/server-bridge-base-client/server-bridge-base-client.d.ts" />
-/// <reference path="src/typings/es6-promise/es6-promise.d.ts" />
 
 declare module "server-bridge-superagent-client" {
     import * as BaseClient from "server-bridge-base-client";
