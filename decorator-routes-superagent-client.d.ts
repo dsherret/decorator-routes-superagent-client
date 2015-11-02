2dee95714ace7791b902312b6d1529b4452cff3a
David Sherret <dsherret@gmail.com>
2015-11-01 21:43:09 -0500

    Removed implicit any in definition file.

--- decorator-routes-superagent-client.d.ts
+++ decorator-routes-superagent-client.d.ts
@@ -5,7 +5,7 @@ declare module "decorator-routes-superagent-client" {
     import * as BaseClient from "decorator-routes-base-client";
 
     export class ClientBase extends BaseClient.BaseClient {
-        protected baseUrl;
+        protected baseUrl: string;
         constructor(baseUrl: string);
         protected get<ReturnType>(url: string, args?: Object): Promise<ReturnType>;
         protected post<SendType, ReturnType>(url: string, sendObj: SendType, args?: Object): Promise<ReturnType>;
