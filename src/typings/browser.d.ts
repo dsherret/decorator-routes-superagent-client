4a69bb1695e88aceaec6cc919ee62389c1b8ff2c
David Sherret <dsherret@gmail.com>
2016-05-18 22:14:27 -0400

    1.1.0 - Upgrade to typings 1.0

--- src/typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/es6-promise/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/superagent/index.d.ts" />
-/// <reference path="browser/definitions/server-bridge-base-client/index.d.ts" />
--- src/typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/es6-promise/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/superagent/index.d.ts" />
-/// <reference path="main/definitions/server-bridge-base-client/index.d.ts" />
